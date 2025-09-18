a5ee913e61e6b14dab0c2854a95e0917da922c44
Singe&Sirene <arnaud88@gmx.fr>
2025-09-18 21:19:55 +0200

    Revert "Merge branch 'main' of https://github.com/UnTrucQuiCode/LaGrosseBerque"
    This reverts commit ed88ce36d3c4caf5322a0db513ae28f05c6ba6f3, reversing
    changes made to 592fa9516da5352a8a72f54b5ce82b4d608f92c4.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,26 +1,6 @@
 """Simplified exports for model package."""
 
-from .souvenir import (
-    BackgroundThought,
-    Context,
-    EmoLvl2ToLv1,
-    Fragment,
-    Link,
-    LinkSouvenir,
-    Souvenir,
-    User,
-    WorkingMemory,
-)
+from .souvenir import Link, LinkSouvenir, Souvenir
 
-__all__ = [
-    "Souvenir",
-    "Link",
-    "LinkSouvenir",
-    "EmoLvl2ToLv1",
-    "Fragment",
-    "Context",
-    "BackgroundThought",
-    "WorkingMemory",
-    "User",
-]
+__all__ = ["Souvenir", "Link", "LinkSouvenir"]
 
